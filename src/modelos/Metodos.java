bb808cc67f09dacd26ac11e306fc267841d7eb8d
Alexissa41 <Alexissa41@gmail.com>
2020-06-04 09:08:24 -0300

    Busqueda por Nro Doc

---
+++ src/modelos/Metodos.java
@@ -0,0 +1,60 @@
+package modelos;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import api.MysqlConnection;
+
+public class Metodos {
+private static int numAfiliado;
+private static int numDocumento;
+
+
+public static int getNumAfiliado() {
+	return numAfiliado;
+}
+public static void setNumAfiliado(int numAfiliado) {
+	Metodos.numAfiliado = numAfiliado;
+}
+public static int getNumDocumento() {
+	return numDocumento;
+}
+public static void setNumDocumento(int numDocumento) {
+	Metodos.numDocumento = numDocumento;
+}
+	
+	ArrayList<String> GetAfiliadoByDocumento() throws ClassNotFoundException {
+
+		ArrayList<String> resultado = new ArrayList<String>();
+		
+		try (Connection con = getConnection())
+	    {
+			String sql = "Select * from tablatest where NROAFI = "+numDocumento;
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next()) {
+				int id = rs.getInt("testDato1");
+				int dato1 = rs.getInt("testDato2");
+				resultado.add(String.valueOf(id));
+				resultado.add(String.valueOf(dato1));
+			}
+			con.close();
+			return resultado;
+	    } catch (SQLException e1) {
+	    	System.out.println("Salio por el catch"); 
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		
+		
+	return resultado;
+}
+	private static Connection getConnection() throws ClassNotFoundException, SQLException {
+		return MysqlConnection.initializeDatabase();
+	}
+}
